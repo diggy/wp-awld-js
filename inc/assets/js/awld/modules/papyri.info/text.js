233a5f88e598e86901deaf94e3b9dbfa5ede942c
diggy <peterherrel@gmail.com>
2012-06-04 21:43:24 +0200

    update papyri module

--- inc/assets/js/awld/modules/papyri.info/text.js
+++ inc/assets/js/awld/modules/papyri.info/text.js
@@ -11,10 +11,11 @@ define(['jquery'], function($) {
         },
         parseData: function(html) {
             var getText = awld.accessor(html);
-            var h3Arr = getText('h3')
+            var h3Arr = getText('h3');
+            var mdtitle = getText('.mdtitle');
             return {
-                name: h3Arr[0],
-                // description: ''
+                name: h3Arr[0] + "- " + mdtitle[0],
+                description: getText('#edition')
             };
         },
     };
