3203695658ae4232898e71bdb8a633e9fed4a1ec
diggy <peterherrel@gmail.com>
2013-03-02 17:09:52 +0100

    0.1.3

--- inc/widgets/widget-init.php
+++ inc/widgets/widget-init.php
@@ -4,14 +4,14 @@
  * 
  * Initialize widget(s).
  *
- * @package		Awld.js for WordPress
- * @category	Widgets
- * @author		Peter J. Herrel
+ * @package     Awld.js for WordPress
+ * @category    Widgets
+ * @author      Peter J. Herrel
  */
 include_once( 'widget-awld-index.php' );
 
+add_action( 'widgets_init', 'wp_awld_js_register_widgets' );
 function wp_awld_js_register_widgets()
 {
-	register_widget( 'Wp_Awld_Js_Widget_Awld_Index' );
+    register_widget( 'Wp_Awld_Js_Widget_Awld_Index' );
 }
-add_action( 'widgets_init', 'wp_awld_js_register_widgets' );
