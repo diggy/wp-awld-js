11c44b9fda58e4a38c708fef8006b0a0e9004c03
diggy <peterherrel@gmail.com>
2014-10-19 22:00:10 +0200

    about

--- inc/admin/admin-about.php
+++ inc/admin/admin-about.php
@@ -31,9 +31,9 @@ function wp_awld_js_settings_about_page()
         <p><?php _e( 'The initial implementation of the library was written by <strong>Nick Rabinowitz</strong>.', 'wp_awld_js' ); ?></p>
         <h4><?php _e( 'Links and resources', 'wp_awld_js' ); ?></h4>
         <ul>
-            <li><?php echo sprintf( __( 'Awld.js <a href="%s" target="_blank">home page</a>', 'wp_awld_js' ), esc_url( 'http://isawnyu.github.com/awld-js/' ) ); ?></li>
+            <li><?php echo sprintf( __( 'Awld.js <a href="%s" target="_blank">home page</a>', 'wp_awld_js' ), esc_url( 'http://isawnyu.github.io/awld-js/' ) ); ?></li>
             <li><?php echo sprintf( __( 'Awld.js <a href="%s" target="_blank">wiki</a>', 'wp_awld_js' ), esc_url( 'https://github.com/isawnyu/awld-js/wiki' ) ); ?></li>
-            <li><?php echo sprintf( __( 'Awld.js code repository on <a href="%s" target="_blank">Github</a>', 'wp_awld_js' ), esc_url( 'http://github.com/isawnyu/awld-js' ) ); ?></li>
+            <li><?php echo sprintf( __( 'Awld.js code repository on <a href="%s" target="_blank">Github</a>', 'wp_awld_js' ), esc_url( 'https://github.com/isawnyu/awld-js' ) ); ?></li>
         </ul>
     </div>
     <div class="welcome-panel-column">
@@ -41,7 +41,7 @@ function wp_awld_js_settings_about_page()
         <p><?php _e( 'Neither the name of the New York University nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.', 'wp_awld_js' ); ?></p>
         <h4><?php _e( 'Copyright', 'wp_awld_js' ); ?></h4>
         <p><?php _e( 'Awld.js is copyright &copy; 2012, Institute for the Study of the Ancient World, New York University. All rights reserved.', 'wp_awld_js' ); ?></p>
-        <p><?php _e( 'WordPress plugin is copyright &copy; 2012, Peter J. Herrel.', 'wp_awld_js' ); ?></p>
+        <p><?php _e( 'WordPress plugin is copyright &copy; 2012-2014, Peter J. Herrel.', 'wp_awld_js' ); ?></p>
         <h4><?php _e( 'License', 'wp_awld_js' ); ?></h4>
         <p><?php echo sprintf( __( 'Awld.js is licensed under the BSD License; see <a href="%s" target="_blank">LICENSE.txt</a> for more infomation.', 'wp_awld_js' ), esc_url( $wp_awld_js->plugin_dir_url . 'inc/assets/js/awld/license/LICENSE.txt' ) ); ?></p>
         <p><?php _e( 'WordPress plugin license: GPLv3.', 'wp_awld_js' ); ?></p>
@@ -52,27 +52,27 @@ function wp_awld_js_settings_about_page()
         <p><?php _e( 'The author has no affiliations with the Institute for the Study of the Ancient World.', 'wp_awld_js' ); ?></p>
         <p><?php echo sprintf(
             __( 'If you like this plugin, feel free to <a href="%1s" target="_blank">rate</a> or <a href="%2s" target="_blank">review</a> it on WordPress.org, or buy the author a <a href="%3s" target="_blank">cup of coffee</a>.', 'wp_awld_js' )
-            ,esc_url( 'http://wordpress.org/extend/plugins/ancient-world-linked-data-for-wordpress/' )
-            ,esc_url( 'http://wordpress.org/support/view/plugin-reviews/ancient-world-linked-data-for-wordpress/' )
+            ,esc_url( 'https://wordpress.org/plugins/ancient-world-linked-data-for-wordpress/' )
+            ,esc_url( 'https://wordpress.org/support/view/plugin-reviews/ancient-world-linked-data-for-wordpress' )
             ,esc_url( 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WAY79HJWYKPQE' )
         ); ?></p>
         <h4><?php _e( 'Links and resources', 'wp_awld_js' ); ?></h4>
         <ul>
-            <li><?php echo sprintf( __( 'Plugin on <a href="%s" target="_blank">WordPress.org</a>', 'wp_awld_js' ), esc_url( 'http://wordpress.org/extend/plugins/ancient-world-linked-data-for-wordpress/' ) ); ?></li>
-            <li><?php echo sprintf( __( 'Plugin <a href="%s" target="_blank">changelog</a>', 'wp_awld_js' ), esc_url( 'http://wordpress.org/extend/plugins/ancient-world-linked-data-for-wordpress/changelog/' ) ); ?></li>
+            <li><?php echo sprintf( __( 'Plugin on <a href="%s" target="_blank">WordPress.org</a>', 'wp_awld_js' ), esc_url( 'https://wordpress.org/plugins/ancient-world-linked-data-for-wordpress/' ) ); ?></li>
+            <li><?php echo sprintf( __( 'Plugin <a href="%s" target="_blank">changelog</a>', 'wp_awld_js' ), esc_url( 'https://wordpress.org/plugins/ancient-world-linked-data-for-wordpress/changelog/' ) ); ?></li>
             <li><?php echo sprintf( __( 'Plugin <a href="%s" target="_blank">support forums</a>', 'wp_awld_js' ), esc_url( 'http://wordpress.org/support/plugin/ancient-world-linked-data-for-wordpress' ) ); ?></li>
             <li><?php echo sprintf( __( 'Plugin code repository on <a href="%s" target="_blank">Github</a>', 'wp_awld_js' ), esc_url( 'https://github.com/diggy/wp-awld-js' ) ); ?></li>
         </ul>
         <h4><?php _e( 'Other plugins', 'wp_awld_js' ); ?></h4>
         <ul>
-            <li><?php echo sprintf( __( '<a href="%s" target="_blank">Pelagios Widgets for WordPress</a>', 'wp_awld_js' ), esc_url( 'http://wordpress.org/extend/plugins/pelagios-widgets-for-wordpress/' ) ); ?></li>
+            <li><?php echo sprintf( __( '<a href="%s" target="_blank">Pelagios Widgets for WordPress</a>', 'wp_awld_js' ), esc_url( 'https://wordpress.org/plugins/pelagios-widgets-for-wordpress/' ) ); ?></li>
         </ul>
     </div>
     </div>
     </div>
     </div>
     <p class="bottomline"><?php _e( 'Disclaimer: the Awld.js project is in the proof-of-concept, pre-alpha, use-at-your-own-risk stage.', 'wp_awld_js' ); ?></p>
-    <p class="bottomline"><?php echo sprintf( __( 'If you encounter an issue with awld.js, please raise a ticket on <a href="%s" target="_blank">Github</a>.', 'wp_awld_js' ), esc_url( 'https://github.com/nrabinowitz/awld-js/issues' ) ); ?></p>
+    <p class="bottomline"><?php echo sprintf( __( 'If you encounter an issue with awld.js, please raise a ticket on <a href="%s" target="_blank">Github</a>.', 'wp_awld_js' ), esc_url( 'https://github.com/isawnyu/awld-js/issues' ) ); ?></p>
     <?php
 }
 }
