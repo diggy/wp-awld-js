4f763d07a9fac2ac4d41c51b999103453d45d0a7
diggy <peterherrel@gmail.com>
2014-10-19 21:59:46 +0200

    widget

--- inc/widgets/widget-awld-index.php
+++ inc/widgets/widget-awld-index.php
@@ -7,45 +7,47 @@
  * @author      Peter J. Herrel
  */
 
-class Wp_Awld_Js_Widget_Awld_Index extends WP_Widget {
-
+class Wp_Awld_Js_Widget_Awld_Index extends WP_Widget
+{
     /** Variables to setup the widget. */
     var $awld_widget_cssclass;
     var $awld_widget_description;
     var $awld_widget_idbase;
     var $awld_widget_name;
-    
+
     /** constructor */
     function Wp_Awld_Js_Widget_Awld_Index()
     {
         /* Widget variable settings. */
-        $this->awld_widget_cssclass = 'widget_awld';
-        $this->awld_widget_description = __( 'The awld.js auto-generated widget collects all the awld.js links in your post content and organizes them by type and source. The widget will only appear if the page contains one or more awld.js shortcodes. Visit the settings tab for configuration options.', 'wp_awld_js' );
-        $this->awld_widget_idbase = 'wp_awld_js_widget_index';
-        $this->awld_widget_name = __('Awld.js Index', 'wp_awld_js' );
-        
+        $this->awld_widget_cssclass     = 'widget_awld';
+        $this->awld_widget_description  = __( 'The awld.js auto-generated widget collects all the awld.js links in your post content and organizes them by type and source. The widget will only appear if the page contains one or more awld.js shortcodes. Visit the settings tab for configuration options.', 'wp_awld_js' );
+        $this->awld_widget_idbase       = 'wp_awld_js_widget_index';
+        $this->awld_widget_name         = __('Awld.js Index', 'wp_awld_js' );
+
         /* Widget settings. */
         $widget_ops = array( 'classname' => $this->awld_widget_cssclass, 'description' => $this->awld_widget_description );
-        
+
         /* Create the widget. */
         $this->WP_Widget('wp_awld_js_index', $this->awld_widget_name, $widget_ops);
     }
     /** @see WP_Widget */
     function widget( $args, $instance )
     {
-        if( is_singular() && has_awld_shortcode( 'awld' ) )
+        global $post;
+
+        if( is_singular() && has_shortcode( $post->post_content, 'awld' ) )
         {
-            extract($args);
-            
+            extract( $args );
+
             $title = $instance['title'];
             $title = apply_filters('widget_title', $title, $instance, $this->id_base);
-            
+
             echo $before_widget;
-            
+
             if ($title) echo $before_title . $title . $after_title;
-            
+
             echo '<div class="awld-index"></div>';
-            
+
             echo $after_widget;
         }
     }
@@ -65,3 +67,5 @@ class Wp_Awld_Js_Widget_Awld_Index extends WP_Widget {
         <?php
     }
 } // Wp_Awld_Js_Widget_Awld_Index
+
+/* end of file widget-awld-index.php */
