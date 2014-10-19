3138b68b2d58ebc0348af3247cd0242f7c7133e7
diggy <peterherrel@gmail.com>
2014-10-19 21:59:57 +0200

    admin init

--- inc/admin/admin-init.php
+++ inc/admin/admin-init.php
@@ -11,13 +11,14 @@
 
 /**
  * Admin Menu
+ *
+ * @return  void
  */
 add_action( 'admin_menu', 'wp_awld_js_admin_menu' );
 function wp_awld_js_admin_menu()
 {
-    global $menu, $wp_awld_js;
-    $main_page = add_options_page(
-        __( 'AWLD Linked Data', 'wp_awld_js' )
+    add_options_page(
+         __( 'AWLD Linked Data', 'wp_awld_js' )
         ,__( 'AWLD Linked Data', 'wp_awld_js' )
         ,'manage_options'
         ,'wp-awld-js-settings'
@@ -27,109 +28,110 @@ function wp_awld_js_admin_menu()
 
 /**
  * Includes
+ *
+ * @return  void
  */
 function wp_awld_js_settings_page()
 {
     include_once( 'admin-forms.php' );
     include_once( 'admin-settings.php' );
+
     wp_awld_js_settings();
 }
 
 /**
- * tinyMCE button
+ * Admin init
+ *
+ * @return  void
  */
-add_filter( 'mce_buttons', 'wp_awld_js_add_buttons_wysiwyg_editor' );
-function wp_awld_js_add_buttons_wysiwyg_editor( $mce_buttons )
+add_action( 'init', 'wp_awld_js_add_mce_button' );
+function wp_awld_js_add_mce_button()
 {
-    $pos = array_search( 'wp_more', $mce_buttons, true );
-    if ( $pos !== false ) {
-        $tmp_buttons = array_slice( $mce_buttons, 0, $pos+1 );
-        $tmp_buttons[] = 'wp_page';
-        $mce_buttons = array_merge( $tmp_buttons, array_slice( $mce_buttons, $pos+1 ) );
-    }
-    return $mce_buttons;
+    if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) )
+        return;
+
+    if ( ! ( get_user_option( 'rich_editing' ) == 'true' && get_option( 'wp_awld_js_button' ) == 'yes' ) )
+        return;
+
+    add_action( 'admin_head',           'wp_awld_js_admin_head' );
+
+    add_filter( 'mce_external_plugins', 'wp_awld_js_mce_external_plugins' );
+    add_filter( 'mce_buttons',          'wp_awld_js_mce_buttons' );
 }
 
-add_action( 'init', 'wp_awld_js_add_shortcode_button' );
-function wp_awld_js_add_shortcode_button()
+/**
+ * Admin CSS
+ *
+ * @return  void
+ */
+function wp_awld_js_admin_head()
 {
-    if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) return;
-    if ( get_user_option( 'rich_editing' ) == 'true' && get_option( 'wp_awld_js_button' ) == 'yes' ) :
-        add_filter( 'mce_external_plugins', 'wp_awld_js_add_shortcode_tinymce_plugin' );
-        add_filter( 'mce_buttons', 'wp_awld_js_register_shortcode_button' );
-    endif;
+    echo '<style type="text/css">i.mce-i-wp-awld-js-mce-button{background-image: url("' . $GLOBALS['wp_awld_js']->plugin_dir_url . 'inc/assets/images/wp_awld_js_icon.png");}</style>' . "\n";
 }
 
-function wp_awld_js_register_shortcode_button( $buttons )
+/**
+ * TinyMCE External Plugins
+ *
+ * @param   array   $plugin_array   array of external plugins
+ * @return  array                   modified array of external plugins
+ */
+function wp_awld_js_mce_external_plugins( $plugin_array )
 {
-    array_push($buttons, "|", "wp_awld_js_shortcodes_button" );
-    return $buttons;
-}
+    $min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-function wp_awld_js_add_shortcode_tinymce_plugin( $plugin_array )
-{
-    global $wp_awld_js;
-    $plugin_array['AwldShortcodes'] = $wp_awld_js->plugin_dir_url . 'inc/assets/js/wp/awld_editor_plugin.js';
+    $plugin_array['wp_awld_js_mce_button'] = $GLOBALS['wp_awld_js']->plugin_dir_url . 'inc/assets/js/wp/awld_editor_plugin' . $min . '.js';
+
     return $plugin_array;
 }
 
-add_filter( 'tiny_mce_version', 'wp_awld_js_refresh_mce' );
-function wp_awld_js_refresh_mce( $ver )
-{
-    $ver += 3;
-    return $ver;
-}
-
 /**
- * Awld.js Add Quicktags to HTML editor
- **/
-if( ! function_exists( '_wp_awld_js_add_quicktags' ) )
-{
-    add_action( 'admin_print_footer_scripts',  '_wp_awld_js_add_quicktags' );
-    function _wp_awld_js_add_quicktags()
-    { 
-        if ( get_option( 'wp_awld_js_quicktags' ) != 'yes' || ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) return;
-        global $pagenow;
-        if( 'post.php' == $pagenow || 'post-new.php' == $pagenow ) :
-        ?>
-        <script type="text/javascript">
-        /* Add custom Quicktag buttons to the editor Wordpress ver. 3.3 and above only
-         *
-         * Params for this are:
-         * - Button HTML ID (required)
-         * - Button display, value="" attribute (required)
-         * - Opening Tag (required)
-         * - Closing Tag (required)
-         * - Access key, accesskey="" attribute for the button (optional)
-         * - Title, title="" attribute (optional)
-         * - Priority/position on bar, 1-9 = first, 11-19 = second, 21-29 = third, etc. (optional)
-         */
-        QTags.addButton( 'awld', 'AWLD', '[awld href=""]', '[/awld]', '', 'default', '991' );
-        QTags.addButton( 'awld_object', 'awld object', '[awld href="" type="object"]', '[/awld]', '', 'object', '992' );
-        QTags.addButton( 'awld_person', 'awld person', '[awld href="" type="person"]', '[/awld]', '', 'person', '993' );
-        QTags.addButton( 'awld_index', 'awld index', '[awld_index]', ' ', '', 'widget', '994' );
-        </script>
-    <?php endif;
-    }
-}
-
-/**
- * Activation
+ * TinyMCE Buttons
+ *
+ * @param   array   $buttons    array of buttons
+ * @return  array               modified array of buttons
  */
-function activate_wp_awld_js()
+function wp_awld_js_mce_buttons( $buttons )
 {
-    update_option( 'wp_awld_js_install', 1 );
-    install_wp_awld_js();
+    array_push( $buttons, "wp_awld_js_mce_button" );
+
+    return $buttons;
 }
 
 /**
- * Install
+ * HTML editor Quicktags
+ *
+ * Add custom Quicktag buttons to the editor Wordpress ver. 3.3 and above only
+ *
+ * Params for this are:
+ *
+ * - Button HTML ID (required)
+ * - Button display, value="" attribute (required)
+ * - Opening Tag (required)
+ * - Closing Tag (required)
+ * - Access key, accesskey="" attribute for the button (optional)
+ * - Title, title="" attribute (optional)
+ * - Priority/position on bar, 1-9 = first, 11-19 = second, 21-29 = third, etc. (optional)
+ *
+ * @return  void
  */
-function install_wp_awld_js()
+add_action( 'admin_print_footer_scripts',  '_wp_awld_js_add_quicktags' );
+function _wp_awld_js_add_quicktags()
 {
-    global $wp_awld_js;
-    wp_awld_js_default_options();
-    update_option( "wp_awld_js_db_version", $wp_awld_js->version );
+    if( get_option( 'wp_awld_js_quicktags' ) != 'yes' || ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) )
+        return;
+
+    global $pagenow;
+
+    if( 'post.php' == $pagenow || 'post-new.php' == $pagenow ) :
+?>
+<script type="text/javascript">
+QTags.addButton( 'awld', 'AWLD', '[awld href=""]', '[/awld]', '', 'default', '991' );
+QTags.addButton( 'awld_object', 'awld object', '[awld href="" type="object"]', '[/awld]', '', 'object', '992' );
+QTags.addButton( 'awld_person', 'awld person', '[awld href="" type="person"]', '[/awld]', '', 'person', '993' );
+QTags.addButton( 'awld_index', 'awld index', '[awld_index]', ' ', '', 'widget', '994' );
+</script>
+<?php
+    endif;
 }
 
 /**
@@ -138,49 +140,17 @@ function install_wp_awld_js()
 function wp_awld_js_default_options()
 {
     global $wp_awld_js_settings;
+
     include_once( 'admin-settings.php' );
-    foreach ( $wp_awld_js_settings as $section ) {
-        foreach ( $section as $value ) {
-            if ( isset( $value['std'] ) && isset( $value['id'] ) ) {
+
+    foreach ( $wp_awld_js_settings as $section )
+    {
+        foreach ( $section as $value )
+        {
+            if ( isset( $value['std'] ) && isset( $value['id'] ) )
                 add_option($value['id'], $value['std']);   
-            }
         }
     }
-}
-
-/**
- * Redirect after activation
- */
-add_action( 'admin_init', 'wp_awld_js_activ_redirect' );
-function wp_awld_js_activ_redirect()
-{
-    if ( get_option( 'wp_awld_js_install' ) == 1 ) :
-        $url = admin_url() . 'options-general.php?page=wp-awld-js-settings&tab=about&activated=1';
-        delete_option( 'wp_awld_js_install' );
-        wp_safe_redirect( $url );
-        exit;
-    endif;
-}
-
-
-/**
- * Deactivation
- */
-function deactivate_wp_awld_js()
-{
-    update_option( 'wp_awld_js_uninstall', 1 );
-    uninstall_wp_awld_js();
-}
-
-/**
- * Uninstall
- */
-function uninstall_wp_awld_js()
-{
-    global $wp_awld_js;
-    wp_awld_js_delete_options();
-    delete_option( "wp_awld_js_db_version", $wp_awld_js->version );
-    delete_option( 'wp_awld_js_uninstall' );
 }
 
 /**
@@ -189,12 +159,17 @@ function uninstall_wp_awld_js()
 function wp_awld_js_delete_options()
 {
     global $wp_awld_js_settings;
+
     include_once( 'admin-settings.php' );
-    foreach ( $wp_awld_js_settings as $section ) {
-        foreach ( $section as $value ) {
-            if ( isset( $value['id'] ) ) {
+
+    foreach ( $wp_awld_js_settings as $section )
+    {
+        foreach ( $section as $value )
+        {
+            if ( isset( $value['id'] ) )
                 delete_option( $value['id'] );
-            }
         }
     }
 }
+
+/* end of file admin-init.php */
